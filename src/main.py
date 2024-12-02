323b9760a02cb404380fdc2da8770740ad7507a7
Leonard Bengtsson <leonard.bengtsson@gmail.com>
2024-12-02 14:54:39 +0100

    Clean up IO

--- src/main.py
+++ src/main.py
@@ -17,35 +17,29 @@ def main():
         ])
     )
 
-    land = input("Välj land:")
-    generations = int(input("Ange antalet generationer:"))
-
     dataset = get_data(Year.Y2020)
-    
+    ims_matrix = dataset.ims_matrix
+    emigration = numpy.sum(ims_matrix, axis=0)
+
+    immigration = numpy.sum(ims_matrix, axis=1)
+    natives = dataset.population - immigration
+    population_migration = math_utils.normalize(ims_matrix + numpy.diag(natives))
+
+    land = input('Choose country: ')
+    assert land in dataset.countries
+    generations = int(input('Choose how many generations: '))
+    assert generations > 0
+
     land_index = dataset.countries.index(land)
     initial_vector = numpy.zeros(len(dataset.countries))
     initial_vector[land_index] = 1
-    
-
-
-
-    ims_matrix = dataset.ims_matrix
-    emigration = numpy.sum(ims_matrix, axis = 0)
-
-    immigration = numpy.sum(ims_matrix, axis = 1)
-    natives = dataset.population - immigration 
-    
-
-    population_migration = math_utils.normalize(ims_matrix + numpy.diag(natives))
 
     result = numpy.linalg.matrix_power(population_migration, generations) @ initial_vector
 
-    tuples = sorted(zip(dataset.countries, result), key=lambda k: k[1])
+    tuples = sorted(zip(dataset.countries, result), key=lambda k: k[1], reverse=True)
     
-    for country, value in zip(dataset.countries, result):
-        print (country, float(value))
-
-
+    for country, value in tuples:
+        print(f'{country}: {round(float(value) * 100, 2)}%')
 
 
 if __name__ == '__main__':
